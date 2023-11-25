8d5a4a396082789bd419f6a5b22b12770957bbb9
Md Christien <mdchristien12@gmail.com>
2023-11-25 12:23:58 +0100

    Build an api for updating issues

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-export const createIssueSchema = z.object({
+export const issueSchema = z.object({
   title: z.string().min(1, "Tittle is required").max(225),
   description: z.string().min(1, "Description is required"),
 });
