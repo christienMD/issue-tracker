d6f3f2c35f367972a670e1493908056dc6a1b141
Md Christien <mdchristien12@gmail.com>
2023-11-27 18:30:23 +0100

    Build an API for Deleting Issues

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,7 +4,7 @@ import delay from "delay";
 import { notFound } from "next/navigation";
 import EditIssueButton from "./EditIssueButton";
 import IssueDetails from "./IssueDetails";
-import DeleteIssueButton from "./edit/DeleteIssueButton";
+import DeleteIssueButton from "./DeleteIssueButton";
 interface Props {
   params: { id: string };
 }
