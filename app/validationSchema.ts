c9044626d8026fe2a44665a3e42e1c72bdc09949
Md Christien <mdchristien12@gmail.com>
2023-11-29 02:21:10 +0100

    Enhance the API to assign issues

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -2,5 +2,20 @@ import { z } from "zod";
 
 export const issueSchema = z.object({
   title: z.string().min(1, "Tittle is required").max(225),
-  description: z.string().min(1, "Description is required"),
+  description: z.string().min(1, "Description is required").max(65535),
+});
+
+export const patchIssueSchema = z.object({
+  title: z.string().min(1, "Tittle is required").max(225).optional(),
+  description: z
+    .string()
+    .min(1, "Description is required")
+    .max(65535)
+    .optional(),
+  assignedToUserId: z
+    .string()
+    .min(1, "AssigedToUserId is Required.")
+    .max(255)
+    .optional()
+    .nullable(),
 });
